e48f01f7c5cbefa93215c6b63302e10d717e7be7
Pau Ponzo <paumultimedial@gmail.com>
2026-03-01 12:02:23 -0300

    experiencia

---
+++ script/experiencia.js
@@ -0,0 +1,175 @@
+// importo las preguntas y respuestas
+import { preguntasYRespuestas } from "./preguntas-y-respuestas.js";
+
+
+// declaro una variable con un arreglo vacio inicial 
+let preguntasSeleccionadas = [];
+
+// declaro la variable puntaje que va a empezar en 0 y va a ir aumentando en medida que la respuesta sea correcta
+let puntaje = 0;
+
+// declaro las constantes, una para preguntas y otra para opciones, seleccionando los elementos del dom donde se va a mostrar
+const contenedorPregunta = document.querySelector('#pregunta')
+
+const contenedorOpciones = document.querySelector('#opciones')
+const contenedorResultado = document.querySelector('#resultado')
+
+
+// selecciono el boton iniciar para detectar el click del usuario
+const btnIniciar = document.querySelector('#btnIniciar');
+
+contenedorPregunta.textContent = "¿Listo para comenzar?";
+// Contenedor opciones queda vacio al iniciar 
+contenedorOpciones.innerHTML = "";
+
+
+
+// Cuando el usuario hace click, se ejecuta la función iniciarJuego
+btnIniciar.addEventListener('click', iniciarJuego);
+
+// defino la funcion 
+function iniciarJuego() {
+    contenedorPregunta.textContent = "Elegí un tema:"; // cambio el contenido del texto de html por elegi una categoria
+    contenedorOpciones.innerHTML = ""; // limpio por si apretan iniciar 2 veces
+    btnIniciar.classList.add("d-none"); // opcional
+
+
+    // este es el arreglo con las dos opciones(claves) que arme en preguntasyrespuestas. El metodo forEach recorre todas las opciones y con la funcion flecha vamos a decir que hacer con cada una de estas opciones
+    Object.keys(preguntasYRespuestas).forEach((opcion) => {
+
+        // y aca al contenedor opciones agrego al html agrego botones dinamicamente segun la opcion
+        contenedorOpciones.innerHTML += `
+    <button class="btn btn-parvati opcion" type="button" data-tema="${opcion}">
+      ${opcion}
+    </button>
+  `;
+
+    });
+
+
+    // declaro otra constante que va a contener el contenedorOpciones y mostrar todas las opciones 
+    const opciones = contenedorOpciones.querySelectorAll('.opcion')
+
+
+    // recorro las opciones y por cada una de estas opciones voy a hacer una accion
+    opciones.forEach((opcion) => {
+        //cuando el usuario haga click en alguna de estas opciones se ejecuta la funcion flecha
+        opcion.addEventListener('click', () => {
+            //Muestra los temas agregando opcion en el html 
+            const tema = opcion.textContent.trim();
+            // cuando haga click en alguna de las opcioknes llamo a la funcion seleccionar tema
+            seleccionarTema(tema)
+
+
+        })
+    })
+}
+
+
+
+// esta es la funcion que va a mostrar la pregunta del indice 0, accedo a la primera
+function seleccionarTema(tema) {
+    preguntasSeleccionadas = preguntasYRespuestas[tema]
+    mostrarPregunta(0)
+
+
+}
+
+
+// invoco esta funciona va a mnostrar la pregunta 0 del indice, accedo a la primer pregunta de la clave pregunta
+function mostrarPregunta(indice) {
+
+    // pregunto si el indice es igual o mayor a preguntas selecionnadas y con el lenght reccorro todo la longitud de preguntasSeleccionadas, para que me tome el indice 4 tambien
+    if (indice >= preguntasSeleccionadas.length) {
+        mostrarResultado();
+        return;
+
+    }
+    //Desestructuro el objeto al que accedemos, digamos que solo dejo la pregunta
+    const { pregunta, respuestaCorrecta, respuestas } = preguntasSeleccionadas[indice]
+    //De las preguntas seleccionadas, agarrá la pregunta número indice y devolveme su texto
+    const textoPregunta = preguntasSeleccionadas[indice].pregunta;
+
+    contenedorPregunta.innerHTML = textoPregunta;
+
+
+    mostrarOpciones(respuestas, respuestaCorrecta, indice);
+}
+
+activarClickRespuestas();
+
+
+
+
+// invoco la funcion que llame cuando se muestran las opciones con activarClickRespuestas
+function activarClickRespuestas() {
+
+    // declaro la constante que va a seleccionar todas las clases respuestas. 
+    const botones = contenedorOpciones.querySelectorAll(".respuesta");
+
+    // recorro todos los botones con foreach y agrego un addeventlistener para que escuche la accion del click 
+    botones.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            // saco selección anterior
+            botones.forEach(b => b.classList.remove("seleccionada"));
+
+            // marco la actual
+            btn.classList.add("seleccionada");
+
+
+        });
+    });
+}
+
+
+
+
+// declaro la funcion que muestra las opciones de cada pregunta, con las valores respuestas, los valores respuesta correcta y el indice para continuar a la proxima pregunta 
+function mostrarOpciones(respuestas, respuestaCorrecta, indice) {
+    //antes de hacer el recorrido , borro el contenido y luego escribe las opciones
+    contenedorOpciones.innerHTML = '';
+    respuestas.forEach((respuesta) => {
+        contenedorOpciones.innerHTML += `<p class="opcion">${respuesta}</p>`;
+
+    })
+
+
+    const opciones = contenedorOpciones.querySelectorAll('.opcion');
+
+    opciones.forEach((opcion) => {
+
+        opcion.addEventListener('click', () => {
+
+            //comparamos lo que el usuario haya hecho click con la respuesta correcta, con el texto que esta dentro de la etiqueta opcion y verifica si opcion es estrictamente correcta a respuestaCorrecta
+            if (opcion.textContent === respuestaCorrecta) {
+
+                // la variable puntaje : 
+                puntaje++
+                
+                //Al elemento opcion le agrego una clase que se llama correcta a traves del metoodo classlist hago lo mismo cuando sea incorrecto(else)
+                opcion.classList.add('correcta')
+
+            } else {
+                opcion.classList.add('incorrecta')
+
+            }
+            //como el codigo se ejecuta inmeditamente le agrego un letardo con el metodo propio de javascript settimeout con dos parametros una funcion flecha que ejectuta el codigo y despues de 500milisegundos pase a la pregunta uno
+            setTimeout(() => {
+                mostrarPregunta(indice + 1)
+            }, 500)
+        })
+    });
+
+}
+
+function mostrarResultado() {
+    // reseteo los contenedores, para que no lo muestre y se vea solo el contenedorResultado
+    contenedorPregunta.innerHTML='';
+    contenedorOpciones.innerHTML='';
+
+    // aca muestro el resultado, colocandole al h2 la variable de puntaje y la variable de preguntasSeleccionadas
+    contenedorResultado.innerHTML = `<h2> Acertaste: ${puntaje} de ${preguntasSeleccionadas.length} </h2>
+    `;
+}
+
+
---
+++ script/preguntas-y-respuestas.js
@@ -0,0 +1,76 @@
+export const preguntasYRespuestas = {
+// Declaro el objeto que almacena las preguntas y respuestas
+
+  // las claves-propiedades inicio y musica representan dos categorias, estas propiedades tiene un array con objetos de preguntas y respuestas y en el objeto respuestas uso un array de cadena de textos.
+
+  inicio: [
+    {
+      pregunta: "Según la portada, ¿cómo se describe a Parvati Records dentro del psytrance?",
+      respuestas: [
+        "Una etiqueta clave reconocida por su estética sonora profunda y conexión con la cultura del trance",
+        "Una radio comercial de música electrónica",
+        "Un club nocturno exclusivo",
+        "Un servicio de venta de entradas"
+      ],
+      respuestaCorrecta: "Una etiqueta clave reconocida por su estética sonora profunda y conexión con la cultura del trance"
+    },
+    {
+      pregunta: "En la sección principal, Parvati Records se presenta como…",
+      respuestas: [
+        "Un sello musical y nada más",
+        "Un encuentro entre arte, naturaleza y conciencia colectiva",
+        "Un concurso de DJs",
+        "Un estudio de grabación"
+      ],
+      respuestaCorrecta: "Un encuentro entre arte, naturaleza y conciencia colectiva"
+    },
+    {
+      pregunta: "Según la portada, ¿qué busca impulsar Parvati desde sus inicios?",
+      respuestas: [
+        "Una estética sonora profunda dentro del psytrance",
+        "Música pop de tendencia",
+        "Canciones cortas de impacto inmediato",
+        "Shows con duración máxima de 30 minutos"
+      ],
+      respuestaCorrecta: "Una estética sonora profunda dentro del psytrance"
+    }
+  ],
+
+
+
+
+  musica: [
+    {
+      pregunta: "Según 'Música', Parvati entiende la música como…",
+      respuestas: [
+        "Un viaje y una exploración colectiva",
+        "Una lista de hits",
+        "Una secuencia de canciones sin contexto",
+        "Un show tradicional"
+      ],
+      respuestaCorrecta: "Un viaje y una exploración colectiva"
+    },
+    {
+      pregunta: "En 'Música', los releases del sello buscan…",
+      respuestas: [
+        "Sostener una progresión que envuelve y transforma",
+        "Impactar rápido",
+        "Ser virales en 10 segundos",
+        "Tener letra y estribillo pegadizo"
+      ],
+      respuestaCorrecta: "Sostener una progresión que envuelve y transforma"
+    },
+    {
+      pregunta: "Según 'Música', la curaduría no es solo selección: es…",
+      respuestas: [
+        "Construcción de identidad",
+        "Marketing",
+        "Publicidad",
+        "Competencia"
+      ],
+      respuestaCorrecta: "Construcción de identidad"
+    }
+  ],
+
+
+}
